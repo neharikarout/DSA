46382f7090781314cdddda68f2b5b0b0416a515a
Neharika Rout <neharikarout01@gmail.com>
2024-01-21 02:28:43 +0530

    Till minimum time que

--- 5. Char arrays & Strings/Char arrays & Strings - 1/main.cpp
+++ 5. Char arrays & Strings/Char arrays & Strings - 1/main.cpp
@@ -149,63 +149,183 @@ using namespace std;
 
 //               STRINGS
 
-bool compare(string a,string b)
-{
-    if(a.length() != b.length())
-    {
-        return false;
-    }
-        int j=0;
-        for(int i=0;i<=a.length();i++)
-        {
-            if(a[i] != b[j])
-            {
-                return false;
-            }
-            j++;
-        }
-    return true;
-}
-
-int main()
-{
-    //string creation
-    // string str;
-    // //cin>>str;
-    // getline(cin,str);
-    // cout<<str<<endl;
-    // cout<<"length "<<str.length()<<endl;
-    // cout<<"is Empty "<<str.empty()<<endl;
-    // cout<<"push back ";str.push_back('A');cout<<str<<endl;
-    //  cout<<"pop back ";str.pop_back();cout<<str<<endl;
-    //  cout<<str.substr(0,3)<<endl;
-
-    //  string a ="Love";
-    //  string b="Lover";
-
-    // //  int ans=a.compare(b);
-    //  int ans2=compare(a,b);
-    //  if(ans2==0)
-    //  {
-    //     cout<<"a and b are same";
-    //  }
-    //  else{
-    //     cout<<"a and b are not same ";
-    //  }
-string sentence= "hello everyone";
-string target="hi";
-
-// cout<<sentence.find(target);
-// if(sentence.find(target) == std::string::npos)
+// bool compare(string a,string b)
 // {
-//     cout<<"Not found"<<endl;
+//     if(a.length() != b.length())
+//     {
+//         return false;
+//     }
+//         int j=0;
+//         for(int i=0;i<=a.length();i++)
+//         {
+//             if(a[i] != b[j])
+//             {
+//                 return false;
+//             }
+//             j++;
+//         }
+//     return true;
 // }
 
-// sentence.replace(0,5,target);
-// cout<<sentence<<endl;
+// int main()
+// {
+//     //string creation
+//     // string str;
+//     // //cin>>str;
+//     // getline(cin,str);
+//     // cout<<str<<endl;
+//     // cout<<"length "<<str.length()<<endl;
+//     // cout<<"is Empty "<<str.empty()<<endl;
+//     // cout<<"push back ";str.push_back('A');cout<<str<<endl;
+//     //  cout<<"pop back ";str.pop_back();cout<<str<<endl;
+//     //  cout<<str.substr(0,3)<<endl;
 
-sentence.erase(0,2);
-cout<<sentence;
+//     //  string a ="Love";
+//     //  string b="Lover";
+
+//     // //  int ans=a.compare(b);
+//     //  int ans2=compare(a,b);
+//     //  if(ans2==0)
+//     //  {
+//     //     cout<<"a and b are same";
+//     //  }
+//     //  else{
+//     //     cout<<"a and b are not same ";
+//     //  }
+// string sentence= "hello everyone";
+// string target="hi";
+
+// // cout<<sentence.find(target);
+// // if(sentence.find(target) == std::string::npos)
+// // {
+// //     cout<<"Not found"<<endl;
+// // }
+
+// // sentence.replace(0,5,target);
+// // cout<<sentence<<endl;
+
+// sentence.erase(0,2);
+// cout<<sentence;
 
 
+
+
+
+//                   CHAR ARRAYS & STRINGS -2
+
+//   REMOVE ALL ADJACENT DUPLICATES IN A STRING
+
+// string removeAjacentDuplicates(string s)
+// {
+//     string ans="";
+//     int i=0;
+//     while(i<s.length())
+//     {
+//         if(ans.length()>0)
+//         {
+
+//         if(ans[ans.length()-1] == s[i])
+//         {
+//             ans.pop_back();
+//         }
+
+//         else{
+//             ans.push_back(s[i]);
+//         }
+//         }
+
+//           else{
+//             ans.push_back(s[i]);
+//         }
+//         i++;
+//     }
+//     return ans;
+
+// }
+
+// string removeOccurences(string s,string part)
+// {
+//     int pos=s.find(part);
+//     while(pos != string::npos)
+// {
+//     s.erase(pos,part.length());
+//     pos=s.find(part);  //looping variable
+// }
+// return s;
+// }
+
+
+//  bool checkPalindrome(string s, int i, int j)
+//  {
+//      while(i<=j)
+//      {
+//     if(s[i]==s[j])
+//     {
+//         i++;
+//         j--;
+//     }
+//     else{
+//         return false;
+//     }
+//      }
+//     return true;
+//  }
+//     bool validPalindrome(string s)
+//      {
+//         int i=0;
+//          int j=s.length()-1;
+//          while(i<=j)
+//        {
+//          if(s[i]==s[j])
+//          {
+//             //following palindriome property so no removal
+//             i++;
+//             j--;
+//          }
+//          else{
+//             //s[i]!=s[j] means either remove i or j
+//             return checkPalindrome(s,i+1,j)||checkPalindrome(s, i, j-1);
+//              }
+//         }
+//         return true;//for 0 removal
+
+//     }  
+
+
+
+// MINIMUM TIME (539) LEETCODE
+//  int findMinDifference(vector<string> timePoints) {
+//     int n = timePoints.size();
+//     vector<int>minutes;
+
+//     // convert time (string) to minutes (int)
+//     for(int i=0; i<n; i++){
+//         string curr = timePoints[i];
+//         int hours = stoi( curr.substr(0,2) );
+//         int mins = stoi( curr.substr(3,2) );
+//         int totalMinutes = hours*60 + mins;
+//         minutes.push_back(totalMinutes);
+//     }
+
+//     sort(minutes.begin(), minutes.end());
+    
+//     int mini = INT_MAX;
+//     for(int i=0; i<n-1; i++){
+//         int diff = minutes[i+1] - minutes[i];
+//         mini = min(mini, diff);
+//     }
+
+//     // for ex - 00:00 and 23:59    
+//     int lastDiff = minutes[0]+1440 - minutes[n-1];      //1440 mins in 24 hours
+//     mini = min(mini, lastDiff);
+
+//     return mini;
+// }
+int main()
+{
+//     string s="adaabcbaabcbc";
+//     string part="abc";
+//      string ans = removeOccurences(s,part);
+//     // string ans=removeAjacentDuplicates(s);
+//     cout<<ans;
 }
