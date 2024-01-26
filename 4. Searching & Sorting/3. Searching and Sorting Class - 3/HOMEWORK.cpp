d01ca859eac42d124b2faa285bc533b9a57636cf
Neharika Rout <neharikarout01@gmail.com>
2024-01-27 00:00:30 +0530

    added

--- 4. Searching & Sorting/3. Searching and Sorting Class - 3/HOMEWORK.cpp
+++ 4. Searching & Sorting/3. Searching and Sorting Class - 3/HOMEWORK.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 //BOOK ALLOCATION PROBLEM
@@ -123,3 +125,62 @@ using namespace std;
 //     int ans = painterPartition(arr,k,n);
 //     cout<<ans;
 // }
+
+
+
+// AGGRESSIVE COWS
+bool isPossible(vector<int> stalls,int k,int mid)
+{
+    int cowCount=1;
+    int lastPosition=stalls[0];
+
+    for(int i=0;i<=stalls.size();i++)
+    {
+        if(stalls[i]-lastPosition>=mid)
+        {
+           cowCount++;
+           if(cowCount==k)
+           {
+            return true;
+           }
+           lastPosition = stalls[i];
+        }
+    }
+    return false;
+
+}
+int aggressiveCows(vector<int> stalls,int k)
+{
+    sort(stalls.begin(),stalls.end());
+    int s=0;
+    int maxi=0;
+    for(int i=0;i<stalls.size();i++)
+    {
+        maxi=max(maxi,stalls[i]);
+    }
+    int e=maxi;
+    int mid=s+(e-s)/2;
+    int ans=0;
+    while(s<=e)
+    {
+        if(isPossible(stalls,k,mid))
+        {
+            ans= mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+
+}
+
+int main()
+{
+    vector<int> stalls={4,3,1,2,6};
+    int ans=aggressiveCows(stalls,2);
+    cout<<ans;
+}
+
