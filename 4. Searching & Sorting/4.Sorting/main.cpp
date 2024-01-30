7e6413c4462ba2137e2ca4bbc4b3541416bcbbd5
Neharika Rout <neharikarout01@gmail.com>
2024-01-30 21:06:27 +0530

    added

--- 4. Searching & Sorting/4.Sorting/main.cpp
+++ 4. Searching & Sorting/4.Sorting/main.cpp
@@ -1,7 +1,55 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+void selectionSort(vector<int> arr)
+{
+    int n=arr.size();
+    
+
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[min]>arr[j])
+            {
+                min=j;
+            }
+        }
+        swap(arr[i],arr[min]);
+    }
+
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+void bubbleSort(vector<int> arr)
+{
+    int n=arr.size();
+
+    for(int i=0;i<n-1;i++)  //for round 1 to n-1
+    {
+        for(int j=0;j<n-i;j++)
+        {
+        
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+     for(int i=0;i<arr.size();i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
 
 int main()
 {
+    vector<int> arr={10,1,7,6,14,9};
+    bubbleSort(arr);
     return 0;
 }
