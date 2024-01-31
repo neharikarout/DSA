a9fc05594edd8dfd94a1861719fc335dee5bb5db
Neharika Rout <neharikarout01@gmail.com>
2024-01-31 13:07:18 +0530

    ...

---
+++ 6. Pointers/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//     int a=7;
+//     // int b=7;
+//     // cout<<a<<endl;
+//     // cout<<&a<<endl; // address of a
+//     // cout<<b<<endl;
+//     // cout<<&b<<endl;
+
+//     //pointer creation
+//     int *p=&a;
+
+//     //access the value using ptr
+//     cout<<*p<<endl;// dereferencing p
+
+
+int a=5;
+
+int *p=&a;
+
+cout<<a<<endl;    //value of a
+cout<<&a<<endl;   //address of a
+cout<<*p<<endl;   //value of variable which it is pointing
+cout<<p<<endl;    //address of variable it is storing
+cout<<&p<<endl;   //address of p
+
+
+}
