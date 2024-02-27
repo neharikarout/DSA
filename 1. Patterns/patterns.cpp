c35747e3c83dbd783bc0fdbf169b0e98d3ac868c
Neharika Rout <neharikarout01@gmail.com>
2024-02-27 11:16:11 +0530

    patterns done

--- 1. Patterns/patterns.cpp
+++ 1. Patterns/patterns.cpp
@@ -228,7 +228,12 @@ void halfDiamond(int n)
         cout<<endl;
     }   
 }
-
+// print 11
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
 void print11(int n)
 {
     for(int i=0;i<n;i++)
@@ -245,6 +250,316 @@ void print11(int n)
     }
 
 }
+
+//print 12
+// 1             1
+// 1 2         2 1
+// 1 2 3     3 2 1
+// 1 2 3 4 4 3 2 1
+void print12(int n)
+{
+    int space = 2*n -2;
+    for(int i=0;i<n;i++)
+    {
+        // Ist triangle
+        for(int j=1;j<=i+1;j++)
+        {
+            cout<<j<<" ";
+        }
+
+        // spaces
+        for(int j=0;j<space;j++)
+        {
+            cout<<"  ";
+        }
+
+        // IInd triangle
+
+        for(int j=i+1;j>0;j--)
+        {
+            cout<<j<<" ";
+        }
+     cout<<endl;
+     space-=2;
+    }
+}
+
+// pattern 13
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+void print13(int n)
+{
+    int count = 1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+}
+
+//Pattern 14
+// A
+// A B
+// A B C
+// A B C D
+void print14(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(char ch = 'A';ch <= 'A' + i; ch++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+//pattern 15
+// A B C D E
+// A B C D
+// A B C
+// A B
+// A
+void print15(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(char j = 'A'; j<='A' + (n-i-1);j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// pattern 16
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+void print16(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        char ch ='A' + i;
+        for(int j=0;j<=i;j++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// alphabet palindromic pyramid
+//         A
+//       A B A
+//     A B C B A 
+//   A B C D C B A
+// A B C D E D C B A
+void alphabetPalindromicPyramid(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        //spaces
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<"  ";
+        }
+
+        //characters
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for(int j=1;j<=2*i+1;j++)
+        {
+            cout<<ch<<" ";
+            if(j<=breakpoint)
+            {
+                ch++;
+            }
+            else{
+                ch--;
+            }
+        }
+
+        cout<<endl;
+    }
+}
+
+//pattern 18
+// E
+// D E
+// C D E
+// B C D E
+// A B C D E
+void print18(int n)
+{
+    for(int i = 0; i<n;i++)
+    {
+       for(char ch = 'E' - i;ch<='E';ch++)
+       {
+         cout<<ch<<" ";
+       }
+        cout<<endl;
+    }
+}
+
+// pattern 19
+// * * * * * * * *
+// * * *     * * *
+// * *         * * 
+// *             *
+                
+// *             *
+// * *         * * 
+// * * *     * * * 
+void print19(int n)
+{
+    //upper half
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<=2*i;j++)
+        {
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+     for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=0;j<2*(n-i);j++)
+        {
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+// butterfly
+// *             *
+// * *         * *
+// * * *     * * *
+// * * * * * * * *
+// * * *     * * *
+// * *         * * 
+// *             *
+void butterfly(int n)
+{
+    //upper half
+    for(int i=1;i<=n;i++)
+    {
+        //stars
+        for(int j=0;j<i;j++)
+        {
+            cout<<"* ";
+        }
+        //spaces
+           for(int j=0;j<2*(n-i);j++)
+        {
+            cout<<"  ";
+        }
+
+        //stars
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    //lower half
+     for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<=2*i;j++)
+        {
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+// hollow square
+// * * * *
+// *     *
+// *     *
+// * * * *
+void hollowSquare(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 || i==n-1)
+            {
+                cout<<"* ";
+            }
+            else{
+                if(j==0 || j==n-1)
+                {
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+        }
+         cout<<endl;
+    }
+}
+
+// pattern 22
+void print22(int n)
+{
+    for(int i=0;i<2*n-1;i++)
+    {
+        for(int j=0;j<2*n-1;j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2*n - 2) - j;
+            int down = (2*n - 2) - i;
+            cout<<(n - min(min(top,down),min(left,right)))<<" ";
+        }
+        cout<<endl;
+    }
+}
+        
 int main()
 {
     int t;
@@ -265,7 +580,18 @@ int main()
         // invertedFullPyramid(n);
         // diamond(n);
         // halfDiamond(n);
-        print11(n);
+        // print11(n);
+        // print12(n);
+        // print13(n);
+        // print14(n);
+        // print15(n);
+        // print16(n);
+        // alphabetPalindromicPyramid(n);
+        // print18(n);
+        // print19(n);
+        // butterfly(n);
+        // hollowSquare(n);
+        print22(n);
     }
 
     
