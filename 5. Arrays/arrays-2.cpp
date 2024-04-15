7149ea7e64c57f116b9cc1c3f5fcd436c9de751f
Neharika Rout <neharikarout01@gmail.com>
2024-04-15 12:44:26 +0530

    ...

--- 5. Arrays/arrays-2.cpp
+++ 5. Arrays/arrays-2.cpp
@@ -138,6 +138,53 @@ vector<int> unionArray(vector<int> a,vector<int> b)
         return unionArr;
     }
 }
+
+
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+    while(i<n1 && j<n2)
+    {
+        if(a[i] <= b[j])
+        {
+            if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else{
+            if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while(i<n1)
+    {
+          if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+    }
+
+    while(j<n2)
+    {
+          if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+    }
+
+     return unionArr;
+}
+
+
+
+
 int main()
 {
     vector<int> a;
