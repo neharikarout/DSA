678330fddf308cf07cfcc76885b9e3f6c8bb67a1
Neharika Rout <neharikarout01@gmail.com>
2024-04-16 18:20:08 +0530

    ...

--- 2. Recursion/6.recursionRevision.cpp
+++ 2. Recursion/6.recursionRevision.cpp
@@ -12,14 +12,40 @@ void bdayReminder(int n)
     {
         cout<<n<<" day left for birthday"<<endl;
     }
+    else{
     cout<<n<<" days left for birthday"<<endl;
-
+    }
     bdayReminder(n-1);
 }
 
+
+void printNto1(int n)
+{
+    if(n==1)
+    {
+        cout<<n<<" ";
+        return;
+    }
+    cout<<n<<" ";
+
+    printNto1(n-1);
+}
+
+void printeven(int i,int n)
+{
+    if(i==n) {
+        cout<<n<<" "<<endl;
+        return;
+    }
+    cout<<n<<" ";
+    printeven(i+2,n);
+}
 int main()
 {
     int n;
     cin>>n;
-    bdayReminder(n);
+    //bdayReminder(n);
+    if(n%2 == 1) n--;
+    // printNto1(10);
+    printeven(2,n);
 }
