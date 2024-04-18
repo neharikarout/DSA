7ac1a93158ea1d17bf199228ad0a50d58c1a1fa5
Neharika Rout <neharikarout01@gmail.com>
2024-04-18 06:52:35 +0530

    array easy module complete

--- 5. Arrays/array-4.cpp
+++ 5. Arrays/array-4.cpp
@@ -32,7 +32,31 @@ int getLongestSubarray(vector<int>& a, int k){
 }
 
 
-// optimal for only positivess
+// optimal for only positives
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int right = 0, left = 0;
+    long long sum = a[0];
+    int  n = a.size();
+    int maxLen = 0;
+    while(right<n)
+    {
+        while(left<=right && sum>k)
+        {
+            sum-=a[left];
+            left++;
+        }
+        
+        if(sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        right++;
+        if(right<n)  sum+=a[right];
+    }
+   return maxLen;
+}
 
 int main()
 {
