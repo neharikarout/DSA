7b84cae050428c044a266388e45934086b8a83a1
Neharika Rout <neharikarout01@gmail.com>
2024-04-20 07:41:02 +0530

    ...

--- 5. Arrays/array-7.cpp
+++ 5. Arrays/array-7.cpp
@@ -22,6 +22,7 @@ vector<int> superiorElements(vector<int>&a) {
 }
 
 
+// better
 int longestSuccessiveElements(vector<int>&a) {
     int longest = 1;
     int currCnt = 0;
@@ -45,6 +46,37 @@ int longestSuccessiveElements(vector<int>&a) {
        longest = max(longest,currCnt);
     }
 return longest;
+}
+
+
+
+// optimal
+int longestConsecutiveSubsequence(vector<int>&a) {
+
+int n = a.size();
+if(n == 0) return 0;
+int longest = 1;
+unordered_set<int> st;
+for(int i = 0 ; i<n ; i++)
+{
+    st.insert(a[i]);
+}
+
+for(auto it:st)
+{
+    if(st.find(it-1) == st.end()){
+        int cnt = 1;
+        int x = it;
+        while(st.find(x+1) != st.end())
+        {
+            x = x+1;
+            cnt = cnt + 1;
+        }
+        longest = max(longest,cnt);
+    }
+}
+
+return  longest;
 }
 
 int main()
