2090c175dd08535774f30714ee4993b97a4414d5
Neharika Rout <neharikarout01@gmail.com>
2024-04-24 00:17:39 +0530

    ...

--- 4. Sorting/sorting-1.cpp
+++ 4. Sorting/sorting-1.cpp
@@ -53,6 +53,25 @@ for(int i=0;i<=n-1;i++){
 }
 }
 
+
+// recursive insertion sort
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
+
+
 int main()
 {
     int n;
