1eff4de82d8d1780117698a6c61a7d6885806d4e
Neharika Rout <neharikarout01@gmail.com>
2024-04-27 06:54:06 +0530

    bs

--- 6. Binary Search/bs-3.cpp
+++ 6. Binary Search/bs-3.cpp
@@ -113,7 +113,7 @@ pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
 }
 
 
-// search in a sorted array
+// search in a  rotated sorted array (unique elements)
 
 int search(vector<int>& arr, int n, int k)
 {
@@ -149,6 +149,48 @@ int search(vector<int>& arr, int n, int k)
     return -1;
 }
 
+// search in sorted array II (have duplicates)
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+       int n = arr.size();
+       int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return true;
+
+        // if low = mid = high
+        if(arr[mid] == arr[low] && arr[mid] == arr[high])
+        {
+            low++;
+            high--;
+            continue;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
 
 
 int main()
