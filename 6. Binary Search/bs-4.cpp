4ba3835eb70b3762eaf0e3fa3d9477fa29f3323d
Neharika Rout <neharikarout01@gmail.com>
2024-04-28 20:23:43 +0530

    ...

--- 6. Binary Search/bs-4.cpp
+++ 6. Binary Search/bs-4.cpp
@@ -38,6 +38,81 @@ int findPeakElement(vector<int> &arr) {
 
 
 
+int floorSqrt(int n)
+{
+    int low = 1;
+    int high = n;
+    // int ans = 1;
+
+    while(low<=high)
+    {
+        long long mid = low + (high - low)/2;
+        long long val = mid * mid;
+
+        if(val > n) 
+        {
+            // eliminate right
+            high = mid -1;
+        }
+
+        else{
+            // ans= mid; // might be answer go to right for larger
+            low = mid + 1;
+        }
+   }
+
+//    return ans;
+      return high;
+}
+
+
+// finding nth root of a number m
+// return 1 if == m
+// return 0 if<m
+// return 2 if>m
+
+int f(int mid , int n , int m)
+{
+  long long ans = 1;
+  for(int i = 1 ; i<=n ; i++)
+  {
+    ans = ans * mid;
+    if(ans > m) return 2;
+  }
+
+  if(ans == m) return 1;
+
+  return 0;
+}
+
+
+int NthRoot(int n, int m) {
+   int low = 1;
+    int high = m;
+
+    while(low<=high)
+    {
+        long long mid = low + (high - low)/2;
+        long long val = f(mid,n,m); 
+
+        if(val == 1) return mid;
+
+        else if(val == 0) 
+        {
+            // eliminate left
+            low = mid + 1;
+        }
+
+        else{ // val > m (return 2)
+            high = mid -1;
+        }
+   }
+
+      return -1;
+}
+
+
+
 int main()
 {
     return 0;
