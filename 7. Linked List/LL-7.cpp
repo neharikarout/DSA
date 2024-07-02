198aa9c7b0a676e1c8f9e49c35e5d9b1af2f4676
Neharika Rout <144371961+neharikarout@users.noreply.github.com>
2024-07-02 07:36:35 +0530

    medium problems of dll

---
+++ 7. Linked List/LL-7.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+
+
+// medium problems of DLL
+
+// delete all occurences of key in dll
+
+class Node {
+  public:
+       int data;
+       Node *prev;
+       Node *next;
+       Node() {
+           this->data = 0;
+           this->prev = NULL;
+           this->next = NULL;
+       }
+       Node(int data) {
+           this->data = data;
+           this->prev = NULL;
+           this->next = NULL;
+       }
+       Node (int data, Node *next, Node *prev) {
+           this->data = data;
+           this->prev = prev;
+           this->next = next;
+       }
+  };
+ 
+
+
+Node * deleteAllOccurrences(Node* head, int k) {
+    Node* temp = head;
+
+    while(temp != NULL){
+        if(temp->data == k)
+        {
+            if(temp == head){
+                head = temp->next;
+            }
+
+            Node* nextNode = temp->next;
+            Node* prevNode = temp->prev;
+
+            if(prevNode != NULL) prevNode->next = nextNode;
+            if(nextNode != NULL) nextNode->prev = prevNode;
+
+            free(temp);
+
+            temp = nextNode;
+        } else {
+
+            temp = temp->next;
+        }
+    }
+
+    return head;
+}
+
+
+// find pairs in dll with given sum
+
+Node* findTail(Node* head)
+{
+    Node* tail = head;
+    while(tail->next != NULL) tail = tail->next;
+
+    return tail;
+}
+
+// vector<pair<int, int>> findPairs(Node* head, int k)
+// {
+//     vector<pair<int,int>> ans;
+
+//     if(head == NULL) return ans;
+
+//     Node* left = head;
+//     Node* right = findTail(head);
+
+//     while(left->data < right->data)
+//     {
+//         if(left->data + right->data == k){
+//           ans.push_back({left->data, right->data});
+//           left = left->next;
+//           right = right->prev;
+//         }
+//         else if(left->data + right->data > k) right = right->prev;
+//         else left = left->next;
+//     }
+
+//     return ans;
+// }
