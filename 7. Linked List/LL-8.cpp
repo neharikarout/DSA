515f66d49d7993b3e5e527052c7dcdf7b504fce8
Neharika Rout <neharikarout01@gmail.com>
2024-07-04 06:27:25 +0530

    flattenning of ll

--- 7. Linked List/LL-8.cpp
+++ 7. Linked List/LL-8.cpp
@@ -140,3 +140,43 @@ Node *rotate(Node *head, int k) {
     return head;
 }
 
+
+
+//  FLATTENNING OF A LL
+
+// Node* merge(Node* list1 , Node* list2){
+// Node* dummyNode = new Node(-1);
+// Node* res = dummyNode;
+
+// while(list1 != NULL && list2 != NULL){
+// 	if(list1->data < list2->data)
+// 	{
+// 		res->child = list1;
+// 		res = list1;
+// 		list1 = list1->child;
+// 	}
+// 	else{
+// 		res->child = list2;
+// 		res = list2;
+// 		list2 = list2->child;
+// 	}
+// 	res->next = NULL;
+// }
+
+// if(list1) res->child = list1;
+// else res->child = list2;
+
+// return dummyNode->child;	
+// }
+
+// Node* flattenLinkedList(Node* head) 
+// {	
+// 	if(head==NULL || head->next==NULL){
+// 		return head;
+// 	}
+	
+// 	Node* mergedHead = flattenLinkedList(head->next);
+
+// 	return merge(head, mergedHead);
+// }
+
