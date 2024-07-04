624e8103706afffae1955c9db48a602a26a668f3
Neharika Rout <neharikarout01@gmail.com>
2024-07-04 07:31:59 +0530

    modify LL-8

--- 7. Linked List/LL-8.cpp
+++ 7. Linked List/LL-8.cpp
@@ -180,3 +180,90 @@ Node *rotate(Node *head, int k) {
 // 	return merge(head, mergedHead);
 // }
 
+
+
+//              COPY LINKED LIST WITH RANDOM PTR AND NEXT
+
+
+//  Node* copyRandomList(Node* head) {
+//         if (head == NULL) {
+
+//         return NULL;
+
+//     }
+
+ 
+
+//     Node *temp = head;
+
+ 
+
+//     // Step 1: Inserting the copied nodes in between the linked list
+
+//     while (temp != NULL) {
+
+//         Node *newNode = new Node(temp->val);
+
+//         newNode->next = temp->next;
+
+//         temp->next = newNode;
+
+//         temp = temp->next->next;
+
+//     }
+
+ 
+
+//     temp = head;
+
+ 
+
+//     // Step 2: Setting random pointers for copied nodes
+
+//     while (temp != NULL) {
+
+//         Node *copyNode = temp->next;
+
+//         if (temp->random != NULL) {
+
+//             copyNode->random = temp->random->next;
+
+//         } else {
+
+//             copyNode->random = NULL;
+
+//         }
+
+//         temp = temp->next->next;
+
+//     }
+
+ 
+
+//     temp = head;
+
+//     Node *dummyNode = new Node(-1);
+
+//     Node *temp2 = dummyNode;
+
+ 
+
+//     // Step 3: Separating the original and copied linked lists
+
+//     while (temp != NULL) {
+
+//         temp2->next = temp->next;
+
+//         temp2 = temp2->next;
+
+//         temp->next = temp->next->next;
+
+//         temp = temp->next; 
+
+//     }
+
+ 
+
+//     return dummyNode->next;
+        
+//     }
